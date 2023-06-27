ca8fc4f970424a4e77481a1e263e86f869f774dd
[thzthix] <seohas0428@naver.com>
2023-06-28 00:46:01 +0900

    done exercise 1.11

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 // a proper place to define a component
 const StatisticLine = (props) => {
   return (
-    <p>
-      {props.text} {props.value}
-    </p>
+    <tr>
+      <td>{props.text} </td>
+      <td>{props.value}</td>
+    </tr>
   );
 };
 const Button = (props) => {
@@ -15,15 +16,15 @@ const Statistics = (props) => {
     return <p>No feedback given</p>;
   }
   return (
-    <div>
+    <table>
       <h2>statistics</h2>
       <StatisticLine text="good" value={props.good} />
       <StatisticLine text="neutral" value={props.neutral} />
       <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.average} />
-      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="all" value={props.all} />
+      <StatisticLine text="average" value={props.average} />
       <StatisticLine text="positive" value={props.positive} />
-    </div>
+    </table>
   );
   // ...
 };
